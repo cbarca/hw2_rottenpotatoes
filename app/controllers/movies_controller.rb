4599149f427dc9cfd4e836e098134ba04f0ec285
saasbook <saasbook@saasbook.(none)>
2012-08-03 16:01:35 -0700

    HW2: new feature - remember the settings - sorting, filtering

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,19 +7,31 @@ class MoviesController < ApplicationController
   end
 
   def index
-  @order = params[:order]
-  @all_ratings = Movie.all_ratings
   @movies = []
-  
-  if params[:ratings] != nil
-    @sel_ratings = params[:ratings].keys
-    @sel_ratings_hash = Hash[@sel_ratings.map { |v| [v, 1] }]
-    @movies = Movie.where(:rating => @sel_ratings)
-    if @order == "title"
+  @sel_order = params[:order]
+  @sel_ratings_hash = params[:ratings]
+  @all_ratings = Movie.all_ratings
+
+  if params[:commit] == 'Refresh'
+    session[:ratings] = @sel_ratings_hash
+  end
+
+  if @sel_ratings_hash != nil
+    @sel_ratings_keys = @sel_ratings_hash.keys
+    #restrict database query to selected filters
+    @movies = Movie.where(:rating => @sel_ratings_keys)
+    if (@sel_order == nil) 
+      @sel_order = session[:order]
+    end
+    if @sel_order == "title"
+      session[:order] = @sel_order
       @movies.sort_by! { |a| a.title }
-    elsif @order == "release_date"
+    elsif @sel_order == "release_date"
+      session[:order] = @sel_order
       @movies.sort_by! { |a| a.release_date }
     end
+  elsif session[:ratings] != nil
+    redirect_to movies_path(:order => session[:order], :ratings => session[:ratings])
   end
   end
 
